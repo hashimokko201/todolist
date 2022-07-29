57a327a122e7f88fba1d28421354489099f0aa32
PC17 <marunimokko@hotmail.com>
2022-07-29 10:18:03 +0900

    first commit

---
+++ locale.rb
@@ -0,0 +1,18 @@
+" ============================================================================
+" Netrw Directory Listing                                        (netrw v156)
+"   /home/pc17/todolist/config/initializers
+"   Sorted by      name
+"   Sort sequence: [\/]$,\<core\%(\.\d\+\)\=\>,\.h$,\.c$,\.cpp$,\~\=\*$,*,\.o$,\.obj$,\.info$,\.swp$,\.bak$,\~$
+"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:special
+" ==============================================================================
+../
+./
+application_controller_renderer.rb
+assets.rb
+backtrace_silencers.rb
+content_security_policy.rb
+cookies_serializer.rb
+filter_parameter_logging.rb
+inflections.rb
+mime_types.rb
+wrap_parameters.rb
